25a5c19878904319fe75e1beaa3af15bc9ff8159
Sophia Clarke <sophiaclarke93@live.com>
2016-08-04 16:34:04 +0100

    Committing game files
    v0.1 of the game- very basic

---
+++ Assets/Scripts/Click.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Click : MonoBehaviour {
+
+    public UnityEngine.UI.Text kpc;
+    public UnityEngine.UI.Text kittyDisplay;
+    public float kitties = 0.00f;
+    public int kittiesPerClick = 1;
+
+    void Update()
+    {
+        kittyDisplay.text = "Kitties: " + kitties;
+        kpc.text = "Kitties Per Click: " + kittiesPerClick;
+    }
+
+    public void Clicked()
+    {
+        kitties += kittiesPerClick;
+    }
+
+}
---
+++ Assets/Scripts/UpgradeManager.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UpgradeManager : MonoBehaviour {
+
+    public Click click;
+    public UnityEngine.UI.Text itemInfo;
+    public float cost;
+    public int count;
+    public int clickPower;
+    public string itemName;
+    private float _newCost;
+
+    void Update()
+    {
+        itemInfo.text = itemName + "\nCost: " + cost + "\nPower: " + clickPower;
+    }
+
+    public void PurchasedUpgrade()
+    {
+        if (click.kitties >= cost)
+        {
+            click.kitties -= cost;
+            count += 1;
+            click.kittiesPerClick += clickPower;
+            cost = Mathf.Round(cost * 1.15f);
+            _newCost = Mathf.Pow(cost, _newCost = cost);
+        }
+    }
+	
+}
